3418a5067793e95da684f501aed99e11410a0b08
catink123 <qwerty.qaz120@gmail.com>
2020-12-10 23:52:38 +0300

    Updates

---
+++ precache-manifest.44575808048c6025f505033d13f6fa4b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7136e73a7c8cd9c6a1d3",
+    "url": "/mngsav/css/app.59eec217.css"
+  },
+  {
+    "revision": "26bb7315f520dc00574e",
+    "url": "/mngsav/css/chunk-vendors.04fa79ae.css"
+  },
+  {
+    "revision": "2bae04cd638afeb9976e8439c67317e1",
+    "url": "/mngsav/index.html"
+  },
+  {
+    "revision": "7136e73a7c8cd9c6a1d3",
+    "url": "/mngsav/js/app.f66400ea.js"
+  },
+  {
+    "revision": "26bb7315f520dc00574e",
+    "url": "/mngsav/js/chunk-vendors.b233c855.js"
+  },
+  {
+    "revision": "2166fe696125ded86945fb84f550eed4",
+    "url": "/mngsav/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/mngsav/robots.txt"
+  }
+]);
